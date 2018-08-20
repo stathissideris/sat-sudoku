9b8e60099013d3a49241b4fb10065b9e45d3002e
Stathis Sideris <stathis@pixelated-noise.com>
2018-08-20 13:48:09 +0300

    Refactor for readability and add 2 more puzzles

--- src/sat/core.clj
+++ src/sat/core.clj
@@ -2,43 +2,43 @@
   (:require [rolling-stones.core :as sat :refer [!]]
             [clojure.string :as str]))
 
-(def rows 9)
-(def cols 9)
-(def values 9)
+(def rows (range 9))
+(def cols (range 9))
+(def values (map inc (range 9)))
 
 
 (defn possible-square-values
   "All the possible values in a square"
   [r c]
-  (for [x (map inc (range values))]
+  (for [x values]
     {:r r :c c :x x}))
 
 
 (defn one-number-per-square []
-  (for [r (range rows)
-        c (range cols)]
+  (for [r rows
+        c cols]
     (sat/exactly 1 (possible-square-values r c))))
 
 
 (defn each-number-once-per-row []
   (apply
    concat
-   (for [row (range rows)]
-     (for [x (map inc (range values))]
+   (for [row rows]
+     (for [x values]
        (sat/exactly
         1
-        (for [c (range cols)]
+        (for [c cols]
           {:r row :c c :x x}))))))
 
 
 (defn each-number-once-per-column []
   (apply
    concat
-   (for [col (range cols)]
-     (for [x (map inc (range values))]
+   (for [col cols]
+     (for [x values]
        (sat/exactly
         1
-        (for [r (range rows)]
+        (for [r rows]
           {:r r :c col :x x}))))))
 
 
@@ -53,7 +53,7 @@
 (defn each-number-once-per-box []
   (apply
    concat
-   (for [x (map inc (range values))]
+   (for [x values]
      (for [d-row (range 0 8 3)
            d-col (range 0 8 3)]
        (sat/exactly
@@ -64,8 +64,8 @@
 
 (defn render [board]
   (let [lookup (zipmap (map (juxt :r :c) board) board)
-        board  (for [r (range rows)
-                     c (range cols)]
+        board  (for [r rows
+                     c cols]
                  {:r r :c c :x (:x (get lookup [r c]))})
         rows   (for [row (partition-by :r board)]
                  (->> (map #(or (:x %) ".") row)
@@ -91,81 +91,122 @@
             (each-number-once-per-box)
             (map vector known)))))
 
-(comment
-  (def puzzle1
-    [{:r 0 :c 1 :x 6}
-     {:r 2 :c 0 :x 4}
+(def puzzle1
+  [{:r 0 :c 1 :x 6}
+   {:r 2 :c 0 :x 4}
 
-     {:r 0 :c 5 :x 7}
-     {:r 2 :c 5 :x 9}
+   {:r 0 :c 5 :x 7}
+   {:r 2 :c 5 :x 9}
 
-     {:r 1 :c 6 :x 8}
-     {:r 0 :c 7 :x 3}
-     {:r 1 :c 8 :x 2}
+   {:r 1 :c 6 :x 8}
+   {:r 0 :c 7 :x 3}
+   {:r 1 :c 8 :x 2}
 
-     {:r 5 :c 0 :x 1}
-     {:r 4 :c 1 :x 3}
-     {:r 4 :c 2 :x 5}
+   {:r 5 :c 0 :x 1}
+   {:r 4 :c 1 :x 3}
+   {:r 4 :c 2 :x 5}
 
-     {:r 3 :c 3 :x 3}
-     {:r 4 :c 3 :x 4}
-     {:r 4 :c 5 :x 8}
-     {:r 5 :c 5 :x 6}
+   {:r 3 :c 3 :x 3}
+   {:r 4 :c 3 :x 4}
+   {:r 4 :c 5 :x 8}
+   {:r 5 :c 5 :x 6}
 
-     {:r 4 :c 6 :x 7}
-     {:r 4 :c 7 :x 9}
-     {:r 3 :c 8 :x 1}
+   {:r 4 :c 6 :x 7}
+   {:r 4 :c 7 :x 9}
+   {:r 3 :c 8 :x 1}
 
-     {:r 7 :c 0 :x 8}
-     {:r 8 :c 1 :x 2}
-     {:r 7 :c 2 :x 3}
+   {:r 7 :c 0 :x 8}
+   {:r 8 :c 1 :x 2}
+   {:r 7 :c 2 :x 3}
 
-     {:r 6 :c 3 :x 2}
-     {:r 8 :c 3 :x 7}
+   {:r 6 :c 3 :x 2}
+   {:r 8 :c 3 :x 7}
 
-     {:r 6 :c 8 :x 7}
-     {:r 8 :c 7 :x 6}]))
+   {:r 6 :c 8 :x 7}
+   {:r 8 :c 7 :x 6}])
 
-(comment
-  (def puzzle2
-    [{:r 0 :c 1 :x 4}
-     {:r 1 :c 0 :x 7}
-     {:r 2 :c 1 :x 9}
-     {:r 2 :c 2 :x 3}
+(def puzzle2 ;;"evil" from websudoku.com
+  [{:r 0 :c 1 :x 4}
+   {:r 1 :c 0 :x 7}
+   {:r 2 :c 1 :x 9}
+   {:r 2 :c 2 :x 3}
 
-     {:r 0 :c 4 :x 8}
+   {:r 0 :c 4 :x 8}
 
-     {:r 1 :c 6 :x 2}
-     {:r 0 :c 7 :x 7}
-     {:r 1 :c 8 :x 9}
+   {:r 1 :c 6 :x 2}
+   {:r 0 :c 7 :x 7}
+   {:r 1 :c 8 :x 9}
 
-     {:r 4 :c 0 :x 4}
-     {:r 4 :c 2 :x 1}
-     {:r 5 :c 2 :x 6}
+   {:r 4 :c 0 :x 4}
+   {:r 4 :c 2 :x 1}
+   {:r 5 :c 2 :x 6}
 
-     {:r 3 :c 3 :x 9}
-     {:r 3 :c 4 :x 2}
-     {:r 5 :c 4 :x 7}
-     {:r 5 :c 5 :x 5}
+   {:r 3 :c 3 :x 9}
+   {:r 3 :c 4 :x 2}
+   {:r 5 :c 4 :x 7}
+   {:r 5 :c 5 :x 5}
 
-     {:r 3 :c 6 :x 6}
-     {:r 4 :c 6 :x 9}
-     {:r 4 :c 8 :x 5}
+   {:r 3 :c 6 :x 6}
+   {:r 4 :c 6 :x 9}
+   {:r 4 :c 8 :x 5}
 
-     {:r 7 :c 0 :x 6}
-     {:r 8 :c 1 :x 8}
-     {:r 7 :c 2 :x 4}
+   {:r 7 :c 0 :x 6}
+   {:r 8 :c 1 :x 8}
+   {:r 7 :c 2 :x 4}
 
-     {:r 8 :c 4 :x 9}
+   {:r 8 :c 4 :x 9}
 
-     {:r 6 :c 6 :x 4}
-     {:r 6 :c 7 :x 3}
-     {:r 7 :c 8 :x 8}
-     {:r 8 :c 7 :x 6}]))
+   {:r 6 :c 6 :x 4}
+   {:r 6 :c 7 :x 3}
+   {:r 7 :c 8 :x 8}
+   {:r 8 :c 7 :x 6}])
+
+
+ ;; this is from the PDF here, "very hard": http://www.sudokuessentials.com/x-wing.html
+(def puzzle3
+  [{:r 0 :c 1 :x 3}
+   {:r 2 :c 0 :x 8}
+
+   {:r 0 :c 3 :x 4}
+   {:r 0 :c 4 :x 8}
+   {:r 1 :c 4 :x 2}
+   {:r 1 :c 5 :x 7}
+   {:r 2 :c 3 :x 3}
+
+   {:r 0 :c 6 :x 6}
+   {:r 0 :c 8 :x 9}
+
+   {:r 3 :c 1 :x 1}
+   {:r 3 :c 2 :x 9}
+   {:r 4 :c 0 :x 7}
+   {:r 4 :c 1 :x 8}
+
+   {:r 4 :c 5 :x 2}
+   {:r 5 :c 5 :x 4}
+
+   {:r 4 :c 7 :x 9}
+   {:r 4 :c 8 :x 3}
+   {:r 5 :c 6 :x 8}
+   {:r 5 :c 7 :x 7}
+
+   {:r 8 :c 0 :x 9}
+   {:r 8 :c 2 :x 2}
+
+   {:r 7 :c 3 :x 1}
+   {:r 7 :c 4 :x 3}
+   {:r 8 :c 4 :x 4}
+   {:r 8 :c 5 :x 8}
+   {:r 6 :c 5 :x 5}
+
+   {:r 8 :c 7 :x 1}
+   {:r 6 :c 8 :x 6}])
 
 (comment
   (render puzzle1)
   (render (solve puzzle1))
 
   (render puzzle2)
-  (render (solve puzzle2)))
+  (render (solve puzzle2))
+
+  (render puzzle3)
+  (render (solve puzzle3)))
